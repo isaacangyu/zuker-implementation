951b12ad976d6efd420945f1d21f255b4533836a
isaacangyu <isaacangyu@gmail.com>
2026-05-01 14:39:33 -0500

    fix errors

--- algos/zuker.py
+++ algos/zuker.py
@@ -7,9 +7,10 @@ class Zuker:
         self.n = len(seq)
         self.lookup = Lookup()
 
-        self.V_matrix = None
-        self.W_matrix = None
-        self.WM_matrix = None
+        self.V = None
+        self.W = None
+        self.WM = None
+        self.W_pointers = None
         self.mfe = None
 
         self.create_V()
@@ -40,8 +41,8 @@ class Zuker:
         return internal
 
     def create_V(self):
-        V_matrix = np.full((self.n, self.n), np.inf)
-        WM_matrix = np.full((self.n, self.n), np.inf)
+        V = np.full((self.n, self.n), np.inf)
+        WM = np.full((self.n, self.n), np.inf)
         m = 3
         for i in range(self.n - 1, -1, -1):
             for j in range(i + m, self.n):
@@ -51,23 +52,24 @@ class Zuker:
                 # non_closed used for the WM calculation after
                 non_closed = np.inf
                 for k in range(i + 1, j):
-                    multiloop = min(multiloop, WM_matrix[i+1, k] + WM_matrix[k+1, j-1] + 9.3)
-                    non_closed = min(non_closed, WM_matrix[i, k] + WM_matrix[k+1, j]) 
+                    multiloop = min(multiloop, WM[i+1, k] + WM[k+1, j-1] + 9.3)
+                    non_closed = min(non_closed, WM[i, k] + WM[k+1, j]) 
                 # hairpin
                 hairpin = self.calc_hairpin(i, j)
                 # stacking
                 stacking = self.calc_stacking(i, j)
                 # internal + buldge loops
-                internal = self.calc_internal(i, j, V_matrix)
+                internal = self.calc_internal(i, j, V)
                 # multiloop
-                # assign V_matrix at (i, j)
-                V_matrix[i, j] = min(hairpin, stacking, internal, multiloop)
-                j_unpaired = WM_matrix[i, j-1]
-                i_unpaired = WM_matrix[i+1, j]
-                closed = V_matrix[i, j] - 0.6
+                # assign V at (i, j)
+                V[i, j] = min(hairpin, stacking, internal, multiloop)
+                # calculate WM
+                j_unpaired = WM[i, j-1]
+                i_unpaired = WM[i+1, j]
+                closed = V[i, j] - 0.6
                 # assign WM at (i, j)
-                WM_matrix[i, j] = min(j_unpaired, i_unpaired, closed, non_closed)
-        self.V_matrix = V_matrix
+                WM[i, j] = min(j_unpaired, i_unpaired, closed, non_closed)
+        self.V = V
 
     def is_valid_base(self, c):
         return str(c) in ('A', 'C', 'G', 'U')
@@ -81,25 +83,25 @@ class Zuker:
         return sp in ('CG', 'AU', 'GU')
 
     def create_W(self):
-        W_matrix = np.zeros((self.n, self.n))
+        W = np.zeros((self.n, self.n))
         for i in range(self.n - 1, -1, -1):
             for j in range(i + 3, self.n):
                 j_paired = np.inf
                 for k in range(i, j - 3):
-                    j_paired = min(j_paired, W_matrix[i, k-1] + self.V[k, j])
-                W_matrix[i, j] = min(W_matrix[i, j-1], j_paired)
-        self.W_matrix = W_matrix
-        self.mfe = self.W_matrix[0, self.n - 1]
+                    j_paired = min(j_paired, W[i, k-1] + self.V[k, j])
+                W[i, j] = min(W[i, j-1], j_paired)
+        self.W = W
+        self.mfe = self.W[0, self.n - 1]
 
-    def W_backtrace(self, W_pointers):
-        node = W_pointers{(1,self.n)}
+    def W_backtrace(self):
+        node = self.W_pointers
         # store (j, 'v/w')
         while node[0] > 1:
-
+            pass
 
 if __name__ == "__main__":
     RNA = 'AUAUAUAU'
     z = Zuker(RNA)
     print('V shape:', z.V.shape)
     print('W shape:', z.W.shape)
-    print('MFE': z.mfe)
+    print('MFE:', z.mfe)
